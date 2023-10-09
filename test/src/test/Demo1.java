062af3d8098b44d1ef50e065a3f75d20e549c804
lve <2662509645@qq.com>
2023-10-09 18:25:09 +0800

    hello world

---
+++ test/src/test/Demo1.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author xiaoming xxx@163.com
+ * @version 2023/10/9 18:24
+ * @since JDK8
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
